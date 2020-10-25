c2b0086439c876ae899316d61df5ad0e3dd708ad
Gary Napier <57505946+napierg@users.noreply.github.com>
2020-10-25 03:07:53 +0000

    Update INSTALL.sh
    Working v0.1

--- INSTALL/INSTALL.sh
+++ INSTALL/INSTALL.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
 
-gitlab_host="http://10.10.20.50"
-gitlab_user="root"
-gitlab_password="C1sco12345"
+gitlab_host="http://192.168.2.242:8080"
 
-echo ""
-printf "Launching Gitlab CE ..."
-docker-compose up -d 2> gitlab_setup.log
+printf "Docker Build of Gitlab CE ..."
+sudo docker-compose --env-file gitlab.env up -d 2> gitlab_setup.log
 
-printf "Waiting for Gitlab CE to become available ."
+printf "\nWaiting for Gitlab CE to become available ."
 until $(curl --output /dev/null --silent --head --fail $gitlab_host); do
     printf '.'
     sleep 10
 done
-
-printf "Configuring external URL for GitLab"
-docker-compose exec gitlab /bin/bash -c "echo external_url $gitlab_host >> /etc/gitlab/gitlab.rb"
-docker-compose exec gitlab gitlab-ctl reconfigure 2>&1 >> gitlab_setup.log
-
-printf "Registering GitLab Runner ... "
-docker-compose exec runner1 gitlab-runner register 2>&1 >> gitlab_setup.log
